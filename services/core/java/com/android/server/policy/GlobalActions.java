502aa995cb1da7e4183f25496ed582aa36300fe0
DvTonder <david.vantonder@gmail.com>
2014-11-08 10:44:21 -0500

    Power menu options
    Squashed commits:
    Author: romanbb
    advanced reboot: add soft reboot option
    https://github.com/CyanogenMod/android_frameworks_base/commit/c301e6c55e5717244235967c550fe51c5adb5d67
    
    Author: invisiblek
    ShutdownThread: set soft reboot trigger accordingly
    https://github.com/CyanogenMod/android_frameworks_base/commit/f094fe6fff0e0adb152528838d4eb1f4bc023175
    
    Author: rmcc
    Method doSoftReboot() from:
    framework: Add advanced reboot options (2 of 2)
    https://github.com/CyanogenMod/android_frameworks_base/commit/8060499ebd529f71fa0bec2dc22cace4f9fa936c

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -103,6 +103,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private static final String GLOBAL_ACTION_KEY_LOCKDOWN = "lockdown";
     private static final String GLOBAL_ACTION_KEY_VOICEASSIST = "voiceassist";
     private static final String GLOBAL_ACTION_KEY_ASSIST = "assist";
+    private static final String GLOBAL_ACTION_KEY_REBOOT = "reboot";
 
     private final Context mContext;
     private final WindowManagerFuncs mWindowManagerFuncs;
@@ -300,6 +301,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mItems.add(getVoiceAssistAction());
             } else if (GLOBAL_ACTION_KEY_ASSIST.equals(actionKey)) {
                 mItems.add(getAssistAction());
+            } else if (GLOBAL_ACTION_KEY_REBOOT.equals(actionKey)) {
+                mItems.add(getRebootAction());
             } else {
                 Log.e(TAG, "Invalid global action key " + actionKey);
             }
@@ -548,6 +551,24 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         };
     }
 
+    private Action getRebootAction() {
+        return new SinglePressAction(com.android.internal.R.drawable.ic_lock_reboot,
+                R.string.global_action_reboot) {
+
+            public void onPress() {
+                mWindowManagerFuncs.reboot();
+            }
+
+            public boolean showDuringKeyguard() {
+                return true;
+            }
+
+            public boolean showBeforeProvisioning() {
+                return true;
+            }
+        };
+    }
+
     private UserInfo getCurrentUser() {
         try {
             return ActivityManagerNative.getDefault().getCurrentUser();
@@ -603,6 +624,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         mAirplaneModeOn.updateState(mAirplaneState);
         mAdapter.notifyDataSetChanged();
         mDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+        mDialog.setTitle(R.string.global_actions);
         if (mShowSilentToggle) {
             IntentFilter filter = new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION);
             mContext.registerReceiver(mRingerModeReceiver, filter);
