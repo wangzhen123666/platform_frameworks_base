a54c9fd6c078e2e2bfccbb67a7acb00b6e719c44
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    systemui: Forward-port notification counters
     * Port from CM11
    
    Conflicts:
    	packages/SettingsProvider/res/values/defaults.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4086,6 +4086,12 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER_PROVIDER = "status_bar_custom_header_provider";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
