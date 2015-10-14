823f6e6c2cbc274946395d22183defa9989a80ea
Alex Cruz <mazdarider23@gmail.com>
2015-10-14 09:33:24 +0000

    Remove demo mode, is useless.......
    Change-Id: Ifbde1b4a78ec38a6bb7711ad1ccd734bae96546a
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -47,8 +47,11 @@ public class TunerFragment extends PreferenceFragment {
     private static final String TAG = "TunerFragment";
 
     private static final String KEY_QS_TUNER = "qs_tuner";
+<<<<<<< HEAD
     private static final String KEY_DEMO_MODE = "demo_mode";
     private static final String KEY_BATTERY_PCT = "battery_pct";
+=======
+>>>>>>> 8ed5bf6... Remove demo mode, is useless.......
 
     public static final String SETTING_SEEN_TUNER_WARNING = "seen_tuner_warning";
 
@@ -75,17 +78,6 @@ public class TunerFragment extends PreferenceFragment {
                 return true;
             }
         });
-        findPreference(KEY_DEMO_MODE).setOnPreferenceClickListener(new OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(android.R.id.content, new DemoModeFragment(), "DemoMode");
-                ft.addToBackStack(null);
-                ft.commit();
-                return true;
-            }
-        });
-        mBatteryPct = (SwitchPreference) findPreference(KEY_BATTERY_PCT);
         if (Settings.Secure.getInt(getContext().getContentResolver(), SETTING_SEEN_TUNER_WARNING,
                 0) == 0) {
             new AlertDialog.Builder(getContext())
