766944df9e77629930e2d0d4c619378f22fb6586
StarkDroid <gamerprince.exp@gmail.com>
2016-03-31 04:56:51 +0200

    fix derp
    Change-Id: I211907f2d41feedc43391559290915282b8b7482

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3560,7 +3560,7 @@ public final class Settings {
             RINGTONE,
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
-            WIFI_AUTO_CONNECT_TYPE
+            WIFI_AUTO_CONNECT_TYPE,
             STATUS_BAR_SHOW_BATTERY_PERCENT,           
             ALLOW_LIGHTS
         };
